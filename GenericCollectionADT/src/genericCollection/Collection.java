07d97864f93d5880b99172d63440e38e75399993
charcoal0163 <202420163@students.asu.edu.jo>
2025-11-08 20:25:40 +0300

    Add files via upload

---
+++ GenericCollectionADT/src/genericCollection/Collection.java
@@ -0,0 +1,73 @@
+package genericCollection;
+public class Collection<T> implements CollectionInterface<T> {
+    private int index = -1;
+    private T[] ar ;
+    public Collection(){
+        ar = (T[]) new Object [10];
+    }
+    public Collection(int arSize) {
+        ar = (T[]) new Object[arSize];
+    }
+    public void add(T element) {
+        if (index < ar.length-1){
+            ++index;
+            ar[index]=element;
+        }
+    }
+    public void remove() {
+        if (index > -1){
+            ar[index] = null;
+            --index;
+        }
+    }
+    public String toString() {
+        String contents = "Collection contains:\n";
+        for (int i = 0; i <= index; i++){
+            contents = contents + ar[i] + "\n";
+        }
+        return contents;
+    }
+    public boolean isEmpty() {
+        return index == -1;
+    }
+    public boolean isFull() {
+        return index == ar.length - 1;
+    }
+    public int size() {
+        return index + 1;
+    }
+    public int count(T element) {
+        System.out.println("== count element (" + element + ") ==");
+        int counter = 0;
+        for (int i = 0; i <= index; i++){
+            if (element.equals(ar[i])){
+                counter++;
+            }
+        }
+        System.out.print("number of occurences of element (" + element + ") is: " );
+        return counter;
+    }
+    public void clear(int loc) {
+        System.out.println("== clear element at location (" + loc + ") ==");
+        if (index > -1){
+            ar[loc] = null;
+        }
+    }
+    public int find(T element) {
+        System.out.println("== find (" + element + ") ==");
+        for (int i = 0; i <= index; i++){
+            if (element.equals(ar[i])){
+                System.out.print("index of first occurence of " + element + " is: ");
+                return i;
+            }
+        }
+        System.out.print("index of first occurence of " + element + " is: ");
+        return -1;
+    }
+    public void update(int index, T newElement) {
+        System.out.println("== update (" + index + ", " + newElement + ") ==");
+        if (index != -1 && index <= ar.length){
+            ar[index] = newElement;
+        }
+    }
+}
---
+++ GenericCollectionADT/src/genericCollection/MyApp.java
@@ -0,0 +1,25 @@
+package genericCollection;
+public class MyApp {
+    public static void main(String[] args) {
+        Collection<Integer> c1 = new Collection<Integer>(6);
+        c1.add(2);
+        c1.add(111);
+        c1.add(-5);
+        c1.add(-9);
+        c1.add(2);
+        c1.add(2);
+        System.out.println(c1);
+        System.out.println(c1.count(2));
+        System.out.println(c1.count(-9));
+        System.out.println(c1.count(999));
+        System.out.println();
+        System.out.println(c1.find(-5));
+        System.out.println(c1.find(-3));
+        System.out.println(c1.find(111));
+        System.out.println();
+        c1.update(0, 88);
+        System.out.println(c1);
+        c1.clear(1);
+        System.out.println(c1);
+    }
+}
