591fb4b6316ec3e1fde3a0e317d06a53c8e5dcd0
charcoal <202420163@students.asu.edu.jo>
2025-12-14 12:39:00 +0300

    Refactor DemoNodes to Linked_List with new structure

--- Linkedpkg/DemoNodes.java
+++ Linkedpkg/DemoNodes.java
@@ -1,43 +1,16 @@
-package Linkedpkg;
-import java.util.Scanner;
-
-public class DemoNodes {
-    
-    
-   
-    
-    
-   public static void main(String[] args) {
-      
-        LLNode<Integer> list = new LLNode<>(10);
-        LLNode<Integer> node1 = new LLNode<>(3);
-        LLNode<Integer> node2 = new LLNode<>(2);
-        LLNode<Integer> node3 = new LLNode<>(6);
-        LLNode<Integer> node4 = new LLNode<>(9);
-        list.setLink(node1);
-        node1.setLink(node2);
-        node2.setLink(node3);
-        node3.setLink(node4);
-        node1=node2=node3=node4=null;
-        // List traversal - print linked list 
+package linked_list;
+public class Linked_List {
+    public static void main(String[] args) {
+        LLNode<Integer> list;
+        LLNode<Integer> first = new LLNode<>(3);
+        LLNode<Integer> second = new LLNode<>(10);
+        LLNode<Integer> third = new LLNode<>(-2);
+        LLNode<Integer> fourth = new LLNode<>(12);
         
-  
-     //   showLinkedList(list);  // add static method 
-        
-     //   System.out.println("The length of list = " + LLLength(list));   // add static method 
-         
-     //   showFirstHalfLL(list);     // add static method 
-        
-        System.out.println("______");
-    //    showSecondHalfLL(list);     // add static method 
-        
-        
-     //   System.out.println("Search " + search1(list, 2));     // add static method 
-        
-        
-        
-        
-        
-      
+        list = first;
+        first.setLink(second);
+        second.setLink(third);
+        third.setLink(fourth);
+        first = second = third = fourth = null;
     }
 }
