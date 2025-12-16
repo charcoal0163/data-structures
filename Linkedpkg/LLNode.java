2fbf541d8d4a6a156175e7dd4c8669658fac3851
charcoal <202420163@students.asu.edu.jo>
2025-12-16 12:36:03 +0300

    Add files via upload

--- Linkedpkg/LLNode.java
+++ Linkedpkg/LLNode.java
@@ -1,22 +1,43 @@
-package linked_list;
-public class LLNode<T> {
-    private T info;
-    private LLNode<T> link;
+
+package Linkedpkg;
     
-    public LLNode(T info){
-        this.info = info;
-        this.link = null;
-    }
-    public T getInfo() {
-        return info;
-    }
-    public void setInfo(T info) {
-        this.info = info;
-    }
-    public LLNode<T> getLink() {
-        return link;
-    }
-    public void setLink(LLNode<T> link) {
-        this.link = link;
-    }
+public class LLNode<T>
+{
+  private LLNode<T> link;
+  private T info;
+  
+  public LLNode(T info)
+  {
+    this.info = info;
+    link = null;
+  }
+ 
+  public void setInfo(T info)
+  // Sets info of this LLNode.
+  {
+    this.info = info;
+  }
+  public T getInfo()
+  // Returns info of this LLONode.
+  {
+    return info;
+  }
+ 
+  public void setLink(LLNode<T> link)
+  // Sets link of this LLNode.
+  {
+    this.link = link;
+  }
+  public LLNode<T> getLink()
+  // Returns link of this LLNode.
+  {
+    return link;
+  }
 }
+    
+    
+      
+    
+  
+  
+
