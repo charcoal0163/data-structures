0e0b41b9f4bd529653aa734dc76c000306f40992
charcoal <202420163@students.asu.edu.jo>
2025-12-16 12:42:28 +0300

    Refactor DemoNodes to input and search linked list
    Refactored DemoNodes to read input values into a linked list and search for a specific value.

--- Linkedpkg/DemoNodes.java
+++ Linkedpkg/DemoNodes.java
@@ -1,43 +1,33 @@
 package Linkedpkg;
 import java.util.Scanner;
-
 public class DemoNodes {
-    
-    
-   
-    
-    
-   public static void main(String[] args) {
-      
-        LLNode<Integer> list = new LLNode<>(10);
-        LLNode<Integer> node1 = new LLNode<>(3);
-        LLNode<Integer> node2 = new LLNode<>(2);
-        LLNode<Integer> node3 = new LLNode<>(6);
-        LLNode<Integer> node4 = new LLNode<>(9);
-        list.setLink(node1);
-        node1.setLink(node2);
-        node2.setLink(node3);
-        node3.setLink(node4);
-        node1=node2=node3=node4=null;
-        // List traversal - print linked list 
-        
-  
-     //   showLinkedList(list);  // add static method 
-        
-     //   System.out.println("The length of list = " + LLLength(list));   // add static method 
-         
-     //   showFirstHalfLL(list);     // add static method 
-        
-        System.out.println("______");
-    //    showSecondHalfLL(list);     // add static method 
-        
-        
-     //   System.out.println("Search " + search1(list, 2));     // add static method 
-        
-        
-        
-        
-        
-      
+    public static void main(String[] args) {
+        System.out.println("nodes: ");
+        Scanner test = new Scanner(System.in);
+        System.out.println("enter values - stop at 999");
+        int x = test.nextInt();
+        LLNode<Integer> list = null;
+        while(x != 999){
+            LLNode<Integer> node = new LLNode<>(x);
+            node.setLink(list);
+            list = node;
+            x = test.nextInt();
+        }
+        LLNode<Integer> key = list;
+        int searched = 20;
+        while(key != null){
+            if(key.getInfo().equals(searched)){
+                System.out.println("yay");
+                break;
+            }
+            key = key.getLink();
+        }
+        if (key == null){
+            System.out.println("lol nope");
+        }
+        //add a new node at the first location
+        LLNode<Integer> nodeJdid = new LLNode<>(3);
+        nodeJdid.setLink(list);
+        list = nodeJdid;
     }
 }
