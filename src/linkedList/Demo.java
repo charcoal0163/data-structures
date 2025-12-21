fa7820843f4872a3f666e4261af720f450966a0d
charcoal <202420163@students.asu.edu.jo>
2025-12-21 12:08:26 +0300

    Remove commented code and clean up Demo.java

--- src/linkedList/Demo.java
+++ src/linkedList/Demo.java
@@ -1,25 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package linkedList;
-
 import java.util.Scanner;
-
-
 public class Demo {
-    
-
     public static void main(String[] args) {
-        // TODO code application logic here
         RefUnsortedList<Integer> d = new RefUnsortedList();
         Scanner key = new Scanner(System.in);
         System.out.println("Enter the number of elements");
         int n = key.nextInt();
-        for(int i =0; i<n; i++)
-        {
+        for(int i =0; i<n; i++) {
             d.add(key.nextInt());
         }
         System.out.println("---------------------");
@@ -33,11 +20,5 @@ public class Demo {
         d.removeMiddle();
         System.out.println("------after remove middle ---------------");
         System.out.println(d);
-        
-        
-        
-        
     }
-   
-    
 }
