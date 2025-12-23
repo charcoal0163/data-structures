1b6bdda3b69a95f6ee5b8f25a11b1647f9f5bf73
charcoal <202420163@students.asu.edu.jo>
2025-12-23 13:00:58 +0300

    Implement max and removeMid methods in Demo
    Added max and removeMid methods to Demo class.

--- linkedList/Demo.java
+++ linkedList/Demo.java
@@ -9,16 +9,56 @@ public class Demo {
         }
         return sum;
     }
+    public static int max(RefUnsortedList<Integer> list){
+        list.reset();
+        int max = list.getNext();
+        for(int i = 0; i < list.size(); i++){
+            int element = list.getNext();
+            if(element > max){
+                max = element;
+            }
+        }
+        return max;
+    }
+    public static <T> void removeMid(RefUnsortedList<T> list){
+        list.reset();
+        int size = list.size() / 2;
+        RefUnsortedList<T> temp = new RefUnsortedList<>();
+        for(int i = 0; i < size; i++){
+            T e  = list.getNext();
+            temp.add(e);
+            list.remove(e);
+        }
+        list.reset();
+        list.remove(list.getNext());
+        temp.reset();
+        while(temp.size() != 0){
+            T e = temp.getNext();
+            list.add(e);
+            temp.remove(e);
+        }
+    }
     public static void main(String[] args) {
         RefUnsortedList<Integer> d = new RefUnsortedList();
-        Scanner key = new Scanner(System.in);
+
+        d.add(3);
+        d.add(-5);
+        d.add(8);
+        d.add(99);
+        d.add(-1);
+        System.out.println(d);
+        removeMid(d);
+        System.out.println(d);
+        
+/*        Scanner key = new Scanner(System.in);
         System.out.println("Enter the number of elements");
         int n = key.nextInt();
-        for(int i =0; i<n; i++) {
+        for(int i =0; i<n; i++){
             d.add(key.nextInt());
         }
         System.out.println("---------------------");
         System.out.println(d);
+/*
         d.addLast(600);
         System.out.println("-----after add last----------------");
         System.out.println(d);
@@ -28,5 +68,6 @@ public class Demo {
         d.removeMiddle();
         System.out.println("------after remove middle ---------------");
         System.out.println(d);
+*/
     }
 }
