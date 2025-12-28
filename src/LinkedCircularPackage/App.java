a35a1a54db44facfdd8b85e1e120ef32df88c139
charcoal <202420163@students.asu.edu.jo>
2025-12-28 12:10:58 +0300

    Add files via upload

---
+++ src/LinkedCircularPackage/App.java
@@ -0,0 +1,64 @@
+package LinkedCircularPackage;
+
+public class App {
+  
+        public static void main(String[] args) {
+        LLNode<Integer> circularList = null;
+
+        //How to add a new node at the begining of a circular linked list?
+        // .................................
+
+
+
+        //How to print all elements in a Circular linked list?
+        System.out.println("Printing all elements in a Circular linked list:");
+        // ..................................
+
+
+
+
+
+        //How to delete the first node from a circular linked list?
+        System.out.println("Delete the first node from a circular linked list:");
+        // ..................................
+
+
+
+
+
+
+
+        // Reinitialize the circularList.
+        circularList = null;
+
+        //How to add a new node at the end of a circular linked list?
+        // ..................................
+
+
+
+
+
+        //How to print all elements in a Circular linked list?
+        // ..................................
+
+
+
+
+        //How to delete the Last node from a circular linked list?
+        System.out.println("Delete the last node from a circular linked list:");
+        // ..................................
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+    
+}
