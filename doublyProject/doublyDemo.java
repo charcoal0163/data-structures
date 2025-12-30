77e724dbb4f14fe4c20fdb6fee4d698f05f16578
charcoal <202420163@students.asu.edu.jo>
2025-12-30 13:01:51 +0300

    Modify doublyDemo to include print functionality
    Updated the values of DLLNode instances and added forward and backward print functionality.

--- doublyProject/doublyDemo.java
+++ doublyProject/doublyDemo.java
@@ -1,21 +1,46 @@
 package javaapplication216;
 public class doublyDemo {
     public static void main(String[] args) {
-        DLLNode<Integer> one = new DLLNode<>(3);
-        DLLNode<Integer> two = new DLLNode<>(5);
+        DLLNode<Integer> one = new DLLNode<>(9);
+        DLLNode<Integer> two = new DLLNode<>(-4);
         DLLNode<Integer> first = new DLLNode<>(7);
-        DLLNode<Integer> last = new DLLNode<>(9);
+        DLLNode<Integer> last = new DLLNode<>(3);
         
-        //linking first and one
+        //linking first and one: first <--> one
         first.setFront(one);
         one.setBack(first);
         
-        //linking one and two
+        //linking one and two: one <--> two
         one.setFront(two);
         two.setBack(one);
         
-        //linking two and last
+        //linking two and last: two <--> last
         two.setFront(last);
         last.setBack(two);
+        one = two = null;
+        
+        //forward print
+        DLLNode<Integer> point = first;
+        while(point != null){
+            System.out.print(point.getInfo() + "  ");
+            point = point.getFront();
+        }
+        
+        //backward print: version 1 (without last)
+        DLLNode<Integer> pointer = first;
+        while(pointer.getFront() != null){
+            pointer = pointer.getFront();
+        }
+        while(pointer != null){
+            System.out.println(pointer.getInfo() + "  ");
+            pointer = pointer.getBack();
+        }
+        
+        //backward print: version 2 (with last)
+        DLLNode<Integer> visit = last;
+        while(visit != null){
+            System.out.println(visit.getInfo() + "  ");
+            visit = visit.getBack();
+        }
     }
 }
