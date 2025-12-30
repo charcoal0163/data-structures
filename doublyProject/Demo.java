6f48c42995e66955def5f7a82d01829ba581d071
charcoal <202420163@students.asu.edu.jo>
2025-12-30 12:46:27 +0300

    Update Demo.java

--- doublyProject/Demo.java
+++ doublyProject/Demo.java
@@ -1,6 +1,21 @@
 package javaapplication216;
-public class Demo {
+public class doublyDemo {
     public static void main(String[] args) {
-        // TODO code application logic here
-    }    
+        DLLNode<Integer> one = new DLLNode<>(3);
+        DLLNode<Integer> two = new DLLNode<>(5);
+        DLLNode<Integer> first = new DLLNode<>(7);
+        DLLNode<Integer> last = new DLLNode<>(9);
+        
+        //linking first and one
+        first.setFront(one);
+        one.setBack(first);
+        
+        //linking one and two
+        one.setFront(two);
+        two.setBack(one);
+        
+        //linking two and last
+        two.setFront(last);
+        last.setBack(two);
+    }
 }
