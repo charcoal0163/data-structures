37becb5e64305bdbaf63592e25aae5ebd8da883e
charcoal <202420163@students.asu.edu.jo>
2025-12-30 12:41:28 +0300

    Add files via upload

---
+++ doublyProject/DLLNode.java
@@ -0,0 +1,28 @@
+package javaapplication216;
+public class DLLNode <T> {
+    private T info;
+    private DLLNode<T> front, back;
+    public DLLNode(T info) {
+        this.info = info;
+        back = front = null;
+    }
+    public T getInfo() {
+        return info;
+    }
+    public void setInfo(T info) {
+        this.info = info;
+    }
+    public DLLNode<T> getFront() {
+        return front;
+    }
+    public void setFront(DLLNode<T> front) {
+        this.front = front;
+    }
+    public DLLNode<T> getBack() {
+        return back;
+    }
+    public void setBack(DLLNode<T> back) {
+        this.back = back;
+    }
+    
+}
---
+++ doublyProject/Demo.java
@@ -0,0 +1,6 @@
+package javaapplication216;
+public class Demo {
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }    
+}
