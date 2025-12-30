505c78675a37cba52d026c4446f5df3302610d2d
charcoal <202420163@students.asu.edu.jo>
2025-12-30 13:27:28 +0300

    Refactor doublyDemo to include list utility methods
    Added methods to print the list in both directions and calculate its size.

--- doublyProject/doublyDemo.java
+++ doublyProject/doublyDemo.java
@@ -1,5 +1,52 @@
 package javaapplication216;
 public class doublyDemo {
+    public static <T> void printFwd(DLLNode<T> head){
+        DLLNode<T> point = head;
+        System.out.println("list from top:");
+        while(point != null){
+            System.out.print(point.getInfo() + "  ");
+            point = point.getFront();
+        }
+        System.out.println();
+    }
+    public static <T> void printBwd(DLLNode<T> tail){
+        DLLNode<T> point = tail;
+        System.out.println("list from bottom:");
+        while(point != null){
+            System.out.print(point.getInfo() + "  ");
+            point = point.getBack();
+        }
+        System.out.println();
+    }
+    //print the list regardless of what the function receives (head or tail)
+    public static <T> void print(DLLNode<T> list){
+        DLLNode<T> point = list;
+        if(list.getBack() == null){
+            System.out.println("list from top:");
+            while(point != null){
+                System.out.print(point.getInfo() + "  ");
+                point = point.getFront();
+            }
+            System.out.println();
+        }
+        else{
+            System.out.println("list from bottom:");
+            while(point != null){
+                System.out.print(point.getInfo() + "  ");
+                point = point.getBack();
+            }
+        System.out.println();
+        }
+    }
+    public static <T> int size(DLLNode<T> head){
+        DLLNode<T> point = head;
+        int count = 0;
+        while(point != null){
+            ++count;
+            point = point.getFront();
+        }
+        return count;
+    }
     public static void main(String[] args) {
         DLLNode<Integer> one = new DLLNode<>(9);
         DLLNode<Integer> two = new DLLNode<>(-4);
@@ -25,34 +72,37 @@ public class doublyDemo {
             System.out.print(point.getInfo() + "  ");
             point = point.getFront();
         }
-        
+        System.out.println();
         //backward print: version 1 (without last)
         DLLNode<Integer> pointer = first;
         while(pointer.getFront() != null){
             pointer = pointer.getFront();
         }
         while(pointer != null){
-            System.out.println(pointer.getInfo() + "  ");
+            System.out.print(pointer.getInfo() + "  ");
             pointer = pointer.getBack();
         }
-        
+        System.out.println();
         //backward print: version 2 (with last)
         DLLNode<Integer> visit = last;
         while(visit != null){
-            System.out.println(visit.getInfo() + "  ");
+            System.out.print(visit.getInfo() + "  ");
             visit = visit.getBack();
         }
-
+        System.out.println();
         //add new node at the beginning
         DLLNode<Integer> jdide = new DLLNode<>(12);
         jdide.setFront(first);
         first.setBack(jdide);
         first = jdide;
-        
         //add new node at the end
         DLLNode<Integer> jdide2 = new DLLNode<>(-2);
         jdide2.setBack(last);
         last.setFront(jdide2);
         last = jdide2;
+        
+        print(first);
+        print(last);
+        System.out.println(size(first));
     }
 }
